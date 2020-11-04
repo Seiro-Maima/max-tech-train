35d1ce2759f79e9ed90593eb705a2a01b69886c0
Ommaimah Hussein <ommaimah@gmail.com>
2020-11-03 22:19:24 -0800

    added html and csharp folder

---
+++ csharp-instructions/HelloWorld/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            int a = 5;
+            int b = 15;
+            int c = a + b;
+
+            Console.WriteLine("Value of a+b=" + c);
+
+        }
+    }
+}
---
+++ csharp-instructions/Project2-StudentRegistration/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project2_StudentRegistration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Student Registration Form\n");
+
+            Console.Write("Enter first name: ");
+            string FirstName = Console.ReadLine();
+
+            Console.Write("Enter last name: ");
+            string LastName = Console.ReadLine();
+
+            Console.Write("Enter year of birth: ");
+            String BirthYear = Console.ReadLine();
+
+            String TempPassword = FirstName + "*" + BirthYear;
+
+            Console.WriteLine("\nWelcome " + FirstName + " " + LastName + "!");
+            Console.WriteLine("Your registration is complete.");
+            Console.WriteLine("Your temporary password is: " + TempPassword);
+            Console.ReadLine();
+        }
+    }
+}
